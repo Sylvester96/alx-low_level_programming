47a4f456efa6b00db5eb48da3bfa9130653dd86d
Sylvester96 <sylvesterkbr@gmail.com>
2022-06-03 18:31:09 -0700

    Task 8: Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	char digit;
-	 for (digit = '0'; digit <= '9'; digit++)
-		 putchar(digit);
-	 for (digit = 'a'; digit <= 'f'; digit++)
-		 putchar(digit);
-	 putchar('\n');
 
-	 return (0);
+	for (digit = '0'; digit <= '9'; digit++)		
+		putchar(digit);
+	for (digit = 'a'; digit <= 'f'; digit++)
+		putchar(digit);
+	putchar('\n');
+
+	return (0);
 }
