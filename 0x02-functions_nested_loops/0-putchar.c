7f58703ade39d240c2ff6480abeea3251d04511f
Sylvester96 <sylvesterkbr@gmail.com>
2022-06-07 15:28:49 -0700

    Task 0: _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print "_putchar"
+ *
+ * Return; Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for  (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Task 0: A program that prints _putchar, followed by a new line.
+
+Task 1: 
