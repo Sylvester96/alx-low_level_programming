0cf4a9a691b2253925d2c309709c6ea0f4fa8816
Sylvester96 <sylvesterkbr@gmail.com>
2022-06-10 16:42:55 -0700

    Task 9: Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
@@ -19,9 +19,7 @@ int main(void)
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%i", i);
-		if (i != 100)
-			printf(' ');
+			printf("%d", i);
 	}
 	printf("\n");
 	return (0);
