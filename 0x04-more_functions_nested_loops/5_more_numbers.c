fac6d46b98577f74e3cc7f35f0044f16e80497ad
Sylvester96 <sylvesterkbr@gmail.com>
2022-06-09 22:06:56 -0700

    Task 5: Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5_more_numbers.c
+++ 0x04-more_functions_nested_loops/5_more_numbers.c
@@ -8,20 +8,19 @@
 
 void more_numbers(void)
 {
-	char c;
-	int i;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (c = 0; c <= 14; c++)
+		for (j = 0; j < 15; j++)
 		{
-			if (c > 9)
+			if (j >= 10)
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+				_putchar('1');
+				_putchar((j % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
 
