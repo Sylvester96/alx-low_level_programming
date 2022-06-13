3d0ddd478af4cb695d71161e35e9f014e4b414a7
Sylvester96 <sylvesterkbr@gmail.com>
2022-06-13 15:00:29 -0700

    Task 7: Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input string to be cut in half
+ * Return: (0)
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+		i++;
+	i--;
+
+	if (i % 2 == 0)
+		j = (i + 1) / 2;
+	else if (i % 2 != 0)
+		j = i / 2;
+
+	for (j += 1; j <= i; j++)
+		_putchar(str[j]);
+
+	_putchar('\n');
+}
