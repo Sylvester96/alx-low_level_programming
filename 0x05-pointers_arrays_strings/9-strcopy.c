3d3e8ee737614883f93827a3626b2b3cc48705f0
Sylvester96 <sylvesterkbr@gmail.com>
2022-06-13 15:24:59 -0700

    Task 9: strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcopy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination string
+ * @src: string to be copied from
+ * Return: (0)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (src[i] != 0)
+		i++;
+
+	for (; j <= i; j++)
+		dest[j] = scr[j];
+
+	return (dest);
+}
