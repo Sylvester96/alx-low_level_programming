4e7dadc363843abb8f53ed94b85ffc23d2faa7ce
Sylvester96 <sylvesterkbr@gmail.com>
2022-07-19 02:23:09 -0700

    Task 7: get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
- * #head: pointer to the first node in the list
+ * @head: pointer to the first node in the list
  * @index: index of the node to return
  *
  * Return: pointer to the indexed node
