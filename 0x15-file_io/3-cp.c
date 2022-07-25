72b0da17971c056cea3a3d9fcb744aa3f8fde65a
Sylvester96 <sylvesterkbr@gmail.com>
2022-07-25 14:31:07 -0700

    fixed commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,7 +22,7 @@ void check97(int argc)
 
 /**
  * check98 - checks that file_from exists and can be read
- * @check - checks if true or false
+ * @check: checks if true or false
  * @file: file_from name
  * @fd_from: file descriptor of file_from, or -1
  * @fd_to: file descriptor of file_to, or -1
